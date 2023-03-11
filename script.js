956a43c8d2d9216bb79fd30cdb2ce4b8ea78034b
Shessire <125847387+Shessire@users.noreply.github.com>
2023-03-11 09:08:06 +0000

    submit HW

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//Challenge 1
+const myBirthYear = 1998;
+const futureYear = 2026;
+
+myAge = futureYear - myBirthYear;
+
+console.log(myAge);
+
+
+//Challenge 2
+const name = 'Pond';
+let location = 'Thailand';
+let hobby = 'playing video games';
+
+console.log(`Hi, my name is ${name}. I live in ${location} and enjoy ${hobby}.`);
